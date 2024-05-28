ea3ce683258987ebea5914e048dd390eab44a84d
Elbanteny <m.amiryan.p@gmail.com>
2024-05-28 20:10:25 +0700

    first commit

---
+++ data/index.tsx
@@ -0,0 +1,212 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "I prioritize client collaboration, fostering open communication ",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.jpg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I constantly try to improve",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Tech enthusiast with a passion for development.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+
+    {
+        id: 5,
+        title: "Currently learning other JS Framework & Library",
+        description: "The Inside Scoop",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to start a project together?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "ThreadsClone App",
+        des: "ThreadsClone is a cutting-edge social networking app designed to keep you connected with your friends, family, and the world around you. ThreadsClone offers a robust, user-friendly interface for sharing your thoughts, moments, and media seamlessly.",
+        img: "/threads.png",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "c.svg"],
+        link: "https://threads-clone-sage.vercel.app/",
+    },
+    {
+        id: 2,
+        title: "ZoomClone - Video Conferencing App",
+        des: "Simplify your video conferencing experience with Zoom. Seamlessly connect with colleagues and friends.",
+        img: "/zoomClone.png",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        link: "https://zoom-clone-rouge-gamma.vercel.app",
+    },
+    {
+        id: 3,
+        title: "Admin Dashboard with Refine",
+        des: "An Admin dashboard create with Refine,DnD(Drag and drop),antd, and kanban framework, so that makes it more modern and glory.",
+        img: "/adminDashboard.png",
+        iconLists: ["/re.svg", "/antd.svg", "/ts.svg", "/c.svg"],
+        link: "https://refine-admin-dashboard-ten.vercel.app/",
+    },
+    {
+        id: 4,
+        title: "Promptopia - Promp web application",
+        des: "Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts.",
+        img: "/promptopia.png",
+        iconLists: ["/next.svg", "/tail.svg", "/mong.svg"],
+        link: "https://promptopia-euwe3e3e7-mamiryanp-gmailcom.vercel.app/",
+    },
+];
+
+export const testimonials = [
+    {
+        quote:
+            "An amazing web developer! The website created is a marvel: aesthetic, responsive, and full of innovation. Thank you so much!",
+        name: "Chat Gpt 3.5",
+        title: "Open AI Chatbot",
+        image: "/chatgpt.svg"
+    },
+    {
+        quote:
+            "I am very satisfied with the work of Ryan. The website they created met my needs and was completed on time. Ryan was also very communicative and responsive to my questions. I highly recommend Ryan to anyone who needs web development services.",
+        name: "Bard",
+        title: "Google Chatbot",
+        image: "/bard.svg"
+    },
+    {
+        quote:
+            "I am very pleased with the end result you provided. I am very impressed with your skills in developing an attractive and user-friendly website. I also appreciate your hard work and ability to meet deadlines. Thank you very much! 🙏",
+        name: "Bing AI",
+        title: "Microsoft Chatbot",
+        image: "/bing.svg"
+    },
+
+];
+
+export const companies = [
+    {
+        id: 1,
+        name: "cloudinary",
+        img: "/cloud.svg",
+        nameImg: "/cloudName.svg",
+    },
+    {
+        id: 2,
+        name: "appwrite",
+        img: "/app.svg",
+        nameImg: "/appName.svg",
+    },
+    {
+        id: 3,
+        name: "HOSTINGER",
+        img: "/host.svg",
+        nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "stream",
+        img: "/s.svg",
+        nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker.",
+        img: "/dock.svg",
+        nameImg: "/dockerName.svg",
+    },
+];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "Frontend Engineer Intern",
+        desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Mobile App Dev - JSM Tech",
+        desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+        className: "md:col-span-2",
+        thumbnail: "/exp2.svg",
+    },
+    {
+        id: 3,
+        title: "Freelance App Dev Project",
+        desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+        className: "md:col-span-2",
+        thumbnail: "/exp3.svg",
+    },
+    {
+        id: 4,
+        title: "Lead Frontend Developer",
+        desc: "Developed and maintained user-facing features using modern frontend technologies.",
+        className: "md:col-span-2",
+        thumbnail: "/exp4.svg",
+    },
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/Elbanteny"
+    },
+    {
+        id: 2,
+        img: "/insta.svg",
+        link: "https://instagram.com/el_banteny?igshid=ZGUzMzM3NWJiOQ=="
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+        link: "https://www.linkedin.com/in/m-amiryan-p-28543023b"
+    },
+];
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6892d3f3b171af16d615fd2287a0d5b7@o4507306072801280.ingest.de.sentry.io/4507306075422800",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
